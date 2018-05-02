d0f0a04a6677963fdda0e27c00ff8edc9d3a8558
Graham Gilpatric <grahamwithag@gmail.com>
2018-05-02 14:22:46 -0400

    Added seed data for testing purposes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "1")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "1")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "1")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "1")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "2")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "2")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "2")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "3")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "3")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "3")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "3")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "3")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "3")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "4")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "4")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "4")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "4")
+Place.create(name: "My house", street_address: "123 fake street", city: "Springfield", zip: "12345", description: "It's the best", user_id: "1", category: "4")
