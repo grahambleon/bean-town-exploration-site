9ce63954d396c5284fd09efce860e3d549c48df3
Graham Gilpatric <grahamwithag@gmail.com>
2018-05-04 14:16:09 -0400

    It works

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -6,7 +6,7 @@ protect_from_forgery unless: -> { request.format.json? }
   end
 
   def create
-    @review = Review.new(review_params)
+    @review = Review.new(title: review_params[:title], body: review_params[:body], rating: review_params[:rating], place_id: review_params[:place_id], user_id: current_user.id)
     if @review.save!
       flash[:notice] = "Place added successfully"
       render json: @review
@@ -19,6 +19,6 @@ protect_from_forgery unless: -> { request.format.json? }
   private
 
   def review_params
-    params.require(:review).permit(:title, :body, :rating, :place_id, :user_id)
+    params.require(:review).permit(:title, :body, :rating, :place_id)
   end
 end
